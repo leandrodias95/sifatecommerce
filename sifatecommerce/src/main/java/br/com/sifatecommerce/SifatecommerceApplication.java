84d42f82eb8b4050046d7d92f1031dee319da012
leandrodias95 <leandrogit95@gmail.com>
2025-04-26 09:35:10 -0300

    inicialização do projeto

---
+++ sifatecommerce/src/main/java/br/com/sifatecommerce/SifatecommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.sifatecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifatecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifatecommerceApplication.class, args);
+	}
+
+}
---
+++ sifatecommerce/src/test/java/br/com/sifatecommerce/SifatecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sifatecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifatecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
